71fb7f58345650a0a873df72a4888be7a0208598
Hamzaabro5 <hamzaabro683@gmail.com>
2024-06-26 10:19:59 +0500

    Learn Typescript

---
+++ Class-01/app.ts
@@ -0,0 +1,149 @@
+console.log(`Hello Typescript`);
+
+
+// Topics
+
+// Ts Compiler
+// Type Annotation
+// Type Interference
+// Type Aliases
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Ts Compiler
+
+// // Command For Convert ts into js
+
+// tsc app.ts
+// tsc app.ts -w
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Type Annotation
+
+// let username:string = `hamza`
+// username = `usman`
+// console.log(username);
+
+
+// let age:number = 18;
+// age = 12
+// console.log(age);
+
+// let isLoggedIn:boolean = true
+// console.log(isLoggedIn);
+
+
+// let user:string[] = [`hamza`]
+
+
+
+// let username:any = `hamza`
+// username = 18
+// Any is wrong method never use any in ts 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Type Interference
+
+// let username = `hamza`;
+// username = 18; //Error
+// By Default  type string hogae h
+
+
+// let age = 18;
+// age = `hamza`  //error
+// By Default type number hogae h
+
+
+
+// type User = {
+//     username: string,
+//     email: string,
+//     age: number,
+//     isLoggedIn: boolean,
+//     lastname?: string,
+// }
+
+// const user:User = {
+//     username: `hamza`,
+//     email: `Hamzaabro@gmail.com`,
+//     age: 18,
+//     isLoggedIn: true,
+// }
+// user.lastname = `abro`
+
+// console.log(user);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Type Aliases
+
+// type User = string
+// let user:User = `hamza`
+
+// type Fruits = (string | number)[]
+// let fruits:Fruits = [`hamza` , 18]
